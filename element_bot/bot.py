614f50d74b97e5c2f57cdca3c0d3e82bdf7e63cf
vboxuser <clb.brdly2@gmail.com>
2023-03-05 17:53:27 +0000

    fire fixes

--- element_bot/bot.py
+++ element_bot/bot.py
@@ -3,29 +3,34 @@ import nio
 import os
 import fire
 
-@bot.listener.on_custom_event(nio.events.room_events.RoomMessageImage)
-async def on_image_message(room, event):
-    response = await asyn_bot.login("password")
-    print(response)
-    match = botlib.MessageMatch(room, event, bot)
-    if match.is_not_from_this_bot() and room.is_group and room.member_count == 2:
-        url = event.url
-        urllist = url.split("/")
-        response = await asyn_bot.download(urllist[2], urllist[3])
-        fname="./"+event.source['content']['body']
-        with open(fname, 'wb') as binary_file:
-            binary_file.write(response.body)
-            binary_file.close()
-        if isinstance(response, nio.responses.DownloadError):
-            await bot.api.send_text_message(room.room_id, "Bot image retrieval failed. Try again.")
-            pass
-        else:
-            await bot.api.send_image_message(dumproom, fname)
-            os.remove(fname)
-
-def main(serveraddress="http://localhost:8008", botname, password, publicroom):
+def main(botname, password, serveraddress="http://localhost:8008"):
     creds = botlib.Creds(serveraddress, botname, password)
     bot = botlib.Bot(creds)
     asyn_bot = nio.AsyncClient(serveraddress, botname)
-    dumproom = publicroom
+    dumproom = "!AktcLzVyjymxBQBetH:localhost"
+
+    @bot.listener.on_custom_event(nio.events.room_events.RoomMessageImage)
+    async def on_image_message(room, event):
+        response = await asyn_bot.login(password)
+        match = botlib.MessageMatch(room, event, bot)
+        if match.is_not_from_this_bot() and room.is_group and room.member_count == 2:
+            url = event.url
+            urllist = url.split("/")
+            response = await asyn_bot.download(urllist[2], urllist[3])
+            fname="./"+event.source['content']['body']
+            with open(fname, 'wb') as binary_file:
+                binary_file.write(response.body)
+                binary_file.close()
+            if isinstance(response, nio.responses.DownloadError):
+                await bot.api.send_text_message(room.room_id, "Bot image retrieval failed. Try again.")
+                pass
+            else:
+                await bot.api.send_image_message(dumproom, fname)
+                os.remove(fname)
+
     bot.run()
+
+if __name__=="__main__":
+    fire.Fire(main)
+
+
